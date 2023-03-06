996fd5fd6378e3340d0503aa5a037bfad3449a42
pearkid <dexbaen@gmail.com>
2023-03-06 15:36:23 -0600

    made texture assignment for fallingsand2 conditional on blockID

--- src/minecraft/net/minecraft/src/RenderFallingSand2.java
+++ src/minecraft/net/minecraft/src/RenderFallingSand2.java
@@ -13,41 +13,44 @@ public class RenderFallingSand2 extends Render {
 		GL11.glPushMatrix();
 		GL11.glTranslatef((float)d2, (float)d4, (float)d6);
 		this.loadTexture("/DumbStorageTextures/FallingBlock2.png");
-		Block block10;
-		switch (entityFallingSand2.md) {
-			default: block10 = Block.blocksList[1];
-			break;
-			case 1: block10 = Block.blocksList[3];
-			break;
-			case 2: block10 = Block.blocksList[4];
-				break;
-			case 3: block10 = Block.blocksList[5];
-				break;
-			case 4: block10 = Block.blocksList[45];
-				break;
-			case 5: block10 = Block.blocksList[7];
-				break;
-			case 6: block10 = Block.blocksList[12];
-				break;
-			case 7: block10 = Block.blocksList[13];
-				break;
-			case 8: block10 = Block.blocksList[6];
-				break;
-			case 9: block10 = Block.blocksList[14];
-				break;
-			case 10: block10 = Block.blocksList[15];
-				break;
-			case 11: block10 = Block.blocksList[16];
-				break;
-			case 12: block10 = Block.blocksList[48];
-				break;
-			case 13: block10 = Block.blocksList[49];
-				break;
-			case 14: block10 = Block.blocksList[57];
-				break;
-			case 15: block10 = Block.blocksList[41];
-				break;
+		Block block10 = Block.blocksList[81];
+		if (entityFallingSand2.blockID == mod_DumbStorage.snadStoneID) {
+			switch (entityFallingSand2.md) {
+				default: block10 = Block.blocksList[1];
+					break;
+				case 1: block10 = Block.blocksList[3];
+					break;
+				case 2: block10 = Block.blocksList[4];
+					break;
+				case 3: block10 = Block.blocksList[5];
+					break;
+				case 4: block10 = Block.blocksList[45];
+					break;
+				case 5: block10 = Block.blocksList[7];
+					break;
+				case 6: block10 = Block.blocksList[12];
+					break;
+				case 7: block10 = Block.blocksList[13];
+					break;
+				case 8: block10 = Block.blocksList[6];
+					break;
+				case 9: block10 = Block.blocksList[14];
+					break;
+				case 10: block10 = Block.blocksList[15];
+					break;
+				case 11: block10 = Block.blocksList[16];
+					break;
+				case 12: block10 = Block.blocksList[48];
+					break;
+				case 13: block10 = Block.blocksList[49];
+					break;
+				case 14: block10 = Block.blocksList[57];
+					break;
+				case 15: block10 = Block.blocksList[41];
+					break;
+			}
 		}
+
 		World world11 = entityFallingSand2.getWorld();
 		GL11.glDisable(GL11.GL_LIGHTING);
 		this.blocks2.renderBlockFallingSand(block10, world11, MathHelper.floor_double(entityFallingSand2.posX), MathHelper.floor_double(entityFallingSand2.posY), MathHelper.floor_double(entityFallingSand2.posZ));
