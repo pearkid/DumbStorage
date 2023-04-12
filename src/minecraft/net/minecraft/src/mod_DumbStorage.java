904b8d4b3bf34e6ee9f2d9f0ccde65d157c14628
pearkid <dexbaen@gmail.com>
2023-04-12 15:38:21 -0500

    Migrated overrides to constructor

--- src/minecraft/net/minecraft/src/mod_DumbStorage.java
+++ src/minecraft/net/minecraft/src/mod_DumbStorage.java
@@ -10,25 +10,6 @@ public class mod_DumbStorage extends BaseMod {
     public void AddRenderer(@SuppressWarnings("rawtypes") Map map) {
         map.put(EntityFallingSand2.class, new RenderMovingBlock());
     }
-    /*@Override
-    public void ModsLoaded(){
-        Block.class.getDeclaredField(ModLoader.isModLoaded("net.minecraft.src.mod_DumbStorage") ? "redstoneWire" : "aw");
-
-    }
-    static void setFinalStatic(Field field, Object newValue) throws Exception {
-        field.setAccessible(true);
-
-        Field modifiersField = Block.class.getDeclaredField("aw");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-
-        field.set(null, newValue);
-    }
-    public static void main(String args[]) throws Exception {
-        setFinalStatic(BlockRedstoneWire.class.getField("aw"), (new BlockProxyRedstoneWire(55, 164)).setHardness(0.0F).setStepSound(Block.soundPowderFootstep).setBlockName("redstoneDust").disableStats().disableNeighborNotifyOnMetadataChange());
-
-        System.out.format("Everything is %s", false); // "Everything is true"
-    }*/
     public static int nulltex = ModLoader.addOverride("/terrain.png", "/DumbStorageTextures/MissingTexture.png");
     public static int dirttex1 = ModLoader.addOverride("/terrain.png", "/DumbStorageTextures/soloDirts.png");
     public static int dirttex2 = ModLoader.addOverride("/terrain.png", "/DumbStorageTextures/DuoDirts.png");
@@ -64,20 +45,6 @@ public class mod_DumbStorage extends BaseMod {
     public static int monmagtex6 = ModLoader.addOverride("/terrain.png", "/DumbStorageTextures/monmag6.png");
     public static int redblocktex = ModLoader.addOverride("/terrain.png","/DumbStorageTextures/RedstoneBlock.png");
 
-    /*public static int texture0 = 0;
-    public static int texture1 = 1;
-    public static int texture2 = 2;
-    public static int texture3 = 3;
-    public static int texture4 = 4;
-    public static int texture5 = 5;
-    public static int texture6 = 6;
-    public static int texture7 = 7;
-    public static int texture8 = 8;
-    public static int texture9 = 9;
-    public static int texture10 = 10;
-    public static int texture11 = 11;
-    public static int texture12 = 12;*/
-
     @MLProp
     public static int snadStoneID = 255;
     @MLProp
@@ -93,44 +60,12 @@ public class mod_DumbStorage extends BaseMod {
 
 
     public String Version () {
-        return "DumbStorage 0.4.1";
+        return "DumbStorage 0.5.0";
     }
     public mod_DumbStorage(){
 
         new DumbSBlocks();
-
-        Block.blocksList[55] = null;
-        Block redstoneDust = (new BlockProxyRedstoneWire(55, 164)).setHardness(0.0F).setStepSound(Block.soundPowderFootstep).setBlockName("redstoneDust").disableStats().disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[50] = null;
-        Block torchWood = (new BlockProxyTorch(50, 80)).setHardness(0.0F).setLightValue(0.9375F).setStepSound(Block.soundWoodFootstep).setBlockName("torch").disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[75] = null;
-        Block torchRedstoneIdle = (new BlockProxyRedstoneTorch(75, 115, false)).setHardness(0.0F).setStepSound(Block.soundWoodFootstep).setBlockName("notGate").disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[76] = null;
-        Block torchRedstoneActive = (new BlockProxyRedstoneTorch(76, 99, true)).setHardness(0.0F).setLightValue(0.5F).setStepSound(Block.soundWoodFootstep).setBlockName("notGate").disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[93] = null;
-        Block redstoneRepeaterIdle = (new BlockProxyRedstoneRepeater(93, false)).setHardness(0.0F).setStepSound(Block.soundWoodFootstep).setBlockName("diode").disableStats().disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[94] = null;
-        Block redstoneRepeaterActive = (new BlockProxyRedstoneRepeater(94, true)).setHardness(0.0F).setLightValue(0.625F).setStepSound(Block.soundWoodFootstep).setBlockName("diode").disableStats().disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[77] = null;
-        Block button = (new BlockProxyButton(77, Block.stone.blockIndexInTexture)).setHardness(0.5F).setStepSound(Block.soundStoneFootstep).setBlockName("button").disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[96] = null;
-        Block trapdoor = (new BlockProxyTrapDoor(96, Material.wood)).setHardness(3.0F).setStepSound(Block.soundWoodFootstep).setBlockName("trapdoor").disableStats().disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[70] = null;
-        Block pressurePlateStone = (new BlockProxyPressurePlate(70, Block.stone.blockIndexInTexture, EnumMobType.mobs, Material.rock)).setHardness(0.5F).setStepSound(Block.soundStoneFootstep).setBlockName("pressurePlate").disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[72] = null;
-        Block pressurePlatePlanks = (new BlockProxyPressurePlate(72, Block.planks.blockIndexInTexture, EnumMobType.everything, Material.wood)).setHardness(0.5F).setStepSound(Block.soundWoodFootstep).setBlockName("pressurePlate").disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[71] = null;
-        Block doorSteel = (new BlockProxyDoor(71, Material.iron)).setHardness(5.0F).setStepSound(Block.soundMetalFootstep).setBlockName("doorIron").disableStats().disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[64] = null;
-        Block doorWood = (new BlockProxyDoor(64, Material.wood)).setHardness(3.0F).setStepSound(Block.soundWoodFootstep).setBlockName("doorWood").disableStats().disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[69] = null;
-        Block lever = (new BlockProxyLever(69, 96)).setHardness(0.5F).setStepSound(Block.soundWoodFootstep).setBlockName("lever").disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[27] = null;
-        Block railPowered = (new BlockProxyRail(27, 179, true)).setHardness(0.7F).setStepSound(Block.soundMetalFootstep).setBlockName("goldenRail").disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[28] = null;
-        Block railDetector = (new BlockProxyDetectorRail(28, 195)).setHardness(0.7F).setStepSound(Block.soundMetalFootstep).setBlockName("detectorRail").disableNeighborNotifyOnMetadataChange();
-        Block.blocksList[66] = null;
-        Block rail = (new BlockProxyRail(66, 128, false)).setHardness(0.7F).setStepSound(Block.soundMetalFootstep).setBlockName("rail").disableNeighborNotifyOnMetadataChange();
+        new DumbSBlockOverrides();
 
         Item.itemsList[Item.redstone.shiftedIndex] = null;
         Item redstone = (new ItemProxyRedstone(Item.redstone.shiftedIndex - 256)).setIconCoord(8, 3).setItemName("redstone");
