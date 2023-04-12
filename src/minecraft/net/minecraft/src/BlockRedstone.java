7034659188129f48da68371cf32611cdd50ff0be
pearkid <dexbaen@gmail.com>
2023-04-12 16:09:50 -0500

    Undid Migration overrides to constructor and added MLprop settings for block overrides

--- src/minecraft/net/minecraft/src/BlockRedstone.java
+++ src/minecraft/net/minecraft/src/BlockRedstone.java
@@ -66,4 +66,44 @@ public class BlockRedstone extends Block {
         return i1;
     }
 
+    public void randomDisplayTick(World world, int i, int j, int k, Random random) {
+        this.particles(world, i, j, k);
+    }
+    private void particles(World world, int i, int j, int k) {
+        Random random = world.rand;
+        double d = 0.0625D;
+
+        for(int l = 0; l < 6; ++l) {
+            double d1 = (double)((float)i + random.nextFloat());
+            double d2 = (double)((float)j + random.nextFloat());
+            double d3 = (double)((float)k + random.nextFloat());
+            if(l == 0 && !world.isBlockOpaqueCube(i, j + 1, k)) {
+                d2 = (double)(j + 1) + d;
+            }
+
+            if(l == 1 && !world.isBlockOpaqueCube(i, j - 1, k)) {
+                d2 = (double)(j + 0) - d;
+            }
+
+            if(l == 2 && !world.isBlockOpaqueCube(i, j, k + 1)) {
+                d3 = (double)(k + 1) + d;
+            }
+
+            if(l == 3 && !world.isBlockOpaqueCube(i, j, k - 1)) {
+                d3 = (double)(k + 0) - d;
+            }
+
+            if(l == 4 && !world.isBlockOpaqueCube(i + 1, j, k)) {
+                d1 = (double)(i + 1) + d;
+            }
+
+            if(l == 5 && !world.isBlockOpaqueCube(i - 1, j, k)) {
+                d1 = (double)(i + 0) - d;
+            }
+
+            if(d1 < (double)i || d1 > (double)(i + 1) || d2 < 0.0D || d2 > (double)(j + 1) || d3 < (double)k || d3 > (double)(k + 1)) {
+                world.spawnParticle("reddust", d1, d2, d3, 0.0D, 0.0D, 0.0D);
+            }
+        }
+    }
 }
