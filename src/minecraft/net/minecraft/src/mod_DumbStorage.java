282025ed6e6f459d4b90aa71c3590796dcb349a5
pearkid <dexbaen@gmail.com>
2023-04-12 14:08:25 -0500

    added some more redstone block support, still need to finish it

--- src/minecraft/net/minecraft/src/mod_DumbStorage.java
+++ src/minecraft/net/minecraft/src/mod_DumbStorage.java
@@ -111,11 +111,28 @@ public class mod_DumbStorage extends BaseMod {
         Block redstoneRepeaterIdle = (new BlockProxyRedstoneRepeater(93, false)).setHardness(0.0F).setStepSound(Block.soundWoodFootstep).setBlockName("diode").disableStats().disableNeighborNotifyOnMetadataChange();
         Block.blocksList[94] = null;
         Block redstoneRepeaterActive = (new BlockProxyRedstoneRepeater(94, true)).setHardness(0.0F).setLightValue(0.625F).setStepSound(Block.soundWoodFootstep).setBlockName("diode").disableStats().disableNeighborNotifyOnMetadataChange();
+        Block.blocksList[77] = null;
+        Block button = (new BlockProxyButton(77, Block.stone.blockIndexInTexture)).setHardness(0.5F).setStepSound(Block.soundStoneFootstep).setBlockName("button").disableNeighborNotifyOnMetadataChange();
+        Block.blocksList[96] = null;
+        Block trapdoor = (new BlockProxyTrapDoor(96, Material.wood)).setHardness(3.0F).setStepSound(Block.soundWoodFootstep).setBlockName("trapdoor").disableStats().disableNeighborNotifyOnMetadataChange();
+        Block.blocksList[70] = null;
+        Block pressurePlateStone = (new BlockProxyPressurePlate(70, Block.stone.blockIndexInTexture, EnumMobType.mobs, Material.rock)).setHardness(0.5F).setStepSound(Block.soundStoneFootstep).setBlockName("pressurePlate").disableNeighborNotifyOnMetadataChange();
+        Block.blocksList[72] = null;
+        Block pressurePlatePlanks = (new BlockProxyPressurePlate(72, Block.planks.blockIndexInTexture, EnumMobType.everything, Material.wood)).setHardness(0.5F).setStepSound(Block.soundWoodFootstep).setBlockName("pressurePlate").disableNeighborNotifyOnMetadataChange();
+        Block.blocksList[71] = null;
+        Block doorSteel = (new BlockProxyDoor(71, Material.iron)).setHardness(5.0F).setStepSound(Block.soundMetalFootstep).setBlockName("doorIron").disableStats().disableNeighborNotifyOnMetadataChange();
+        Block.blocksList[64] = null;
+        Block doorWood = (new BlockProxyDoor(64, Material.wood)).setHardness(3.0F).setStepSound(Block.soundWoodFootstep).setBlockName("doorWood").disableStats().disableNeighborNotifyOnMetadataChange();
 
         Item.itemsList[Item.redstone.shiftedIndex] = null;
         Item redstone = (new ItemProxyRedstone(Item.redstone.shiftedIndex - 256)).setIconCoord(8, 3).setItemName("redstone");
         Item.itemsList[Item.redstoneRepeater.shiftedIndex] = null;
         Item redstoneRepeater = (new ItemReed(100, Block.redstoneRepeaterIdle)).setIconCoord(6, 5).setItemName("diode");
+        Item.itemsList[Item.doorWood.shiftedIndex] = null;
+        Item doorProxyWood = (new ItemProxyDoor(68, Material.wood)).setIconCoord(11, 2).setItemName("doorWood");
+        Item.itemsList[Item.doorSteel.shiftedIndex] = null;
+        Item doorProxySteel = (new ItemProxyDoor(74, Material.iron)).setIconCoord(12, 2).setItemName("doorIron");
+
         ModLoader.RegisterEntityID(EntityFallingSand2.class, "fallingsand2",FSid);
 
     }
